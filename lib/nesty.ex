4146ec781afd9782e3f96dda78007f191d94a993
Stefan Johnson <sj@scrimpycat.io>
2017-08-25 01:59:05 +1000

    Reimplement get function to combine defaults with key list

--- lib/nesty.ex
+++ lib/nesty.ex
@@ -1,29 +1,23 @@
 defmodule Nesty do
-    def get(data, keys, default \\ nil)
-    def get(data, [_|_], [default|_]) when not is_map(data) and not is_list(data), do: default
-    def get(data, [_|_], default) when not is_map(data) and not is_list(data), do: default
-    def get(data, [key], [default|_]) do
+    def get(data, [{ _, default }|_]) when not is_map(data) and not is_list(data), do: default
+    def get(data, [_|_]) when not is_map(data) and not is_list(data), do: nil
+    def get(data, [{ key, default }]) do
         case data[key] do
             nil -> default
             data -> data
         end
     end
-    def get(data, [key], default) do
+    def get(data, [key]), do: data[key]
+    def get(data, [{ key, default }|keys]) do
         case data[key] do
             nil -> default
-            data -> data
+            data -> get(data, keys)
         end
     end
-    def get(data, [key|keys], [default|defaults]) do
+    def get(data, [key|keys]) do
         case data[key] do
-            nil -> default
-            data -> get(data, keys, defaults)
-        end
-    end
-    def get(data, [key|keys], default) do
-        case data[key] do
-            nil -> default
-            data -> get(data, keys, default)
+            nil -> nil
+            data -> get(data, keys)
         end
     end
 end
